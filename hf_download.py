9cc03c102b4d06519bba91ea64e404fe9a47e85f
Liam Achenbach <liam.achenbach@googlemail.com>
2025-05-10 17:17:28 +0200

    updated hf

--- hf_download.py
+++ hf_download.py
@@ -1,8 +1,7 @@
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 
 
-repo_id = "chengkunli/red_cup_pour"
 dataset = LeRobotDataset(
-    repo_id="chengkunli/red_cup_pour", root="./red_cup_pour"
+    repo_id="LiamFy/red_cup_slim_groot", root="./red_cup_slim_groot"
 )
 
