2de0fc91a6c947a0e22ebcec549de22541000619
Liam Achenbach <liam.achenbach@googlemail.com>
2025-05-10 20:52:01 +0200

    blue slim

--- hf_download.py
+++ hf_download.py
@@ -1,7 +1,11 @@
 from lerobot.common.datasets.lerobot_dataset import LeRobotDataset
 
 
-dataset = LeRobotDataset(
-    repo_id="LiamFy/red_cup_slim_groot", root="./red_cup_slim_groot"
-)
+# dataset = LeRobotDataset(
+#     repo_id="LiamFy/red_cup_slim_groot", root="./red_cup_slim_groot"
+# )
 
+
+dataset = LeRobotDataset(
+    repo_id="LiamFy/blue_slim_groot", root="./blue_cup_pour"
+)
